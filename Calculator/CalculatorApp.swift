c2e7315934b491175be738bec286e50a071c7917
Guillem Fernandez <hello@guillem.cat>
2021-02-24 17:38:21 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Guillermo Fernandez on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
